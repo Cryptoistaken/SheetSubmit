ec13cca69e5c6f322f30f527d36f19bb55665b79
Cryptoistaken <traderspopy@gmail.com>
2026-09-06 08:01:40 +0600

    Replace invisible SoundCloud PageIcon with stroke document icon

--- Pages/src/components/icons/FileTypeIcons.tsx
+++ Pages/src/components/icons/FileTypeIcons.tsx
@@ -26,38 +26,12 @@ export const TwoFaIcon = ({ size = 12, ...props }: { size?: number } & React.SVG
 );
 
 export const PageIcon = ({ size = 12, ...props }: { size?: number } & React.SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 256 145" aria-hidden="true" {...props}>
-    <title>soundcloud</title>
-    <defs>
-      <linearGradient id="PVEm" x1="49.719%" x2="49.719%" y1="-27.701%" y2="100.084%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVIj" x1="50.208%" x2="50.208%" y1="-25%" y2="100.195%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVF3" x1="50.031%" x2="50.031%" y1="-26.166%" y2="100.311%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVJ1" x1="49.936%" x2="49.936%" y1="-23.196%" y2="100.193%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVu9" x1="49.525%" x2="49.525%" y1="-89.845%" y2="101.504%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVlH" x1="50.151%" x2="50.151%" y1="-13.846%" y2="100.179%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVcG" x1="49.659%" x2="49.659%" y1="-95.238%" y2="100.836%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PV9b" x1="49.596%" x2="49.596%" y1="-51.09%" y2="100.373%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVb7" x1="50.414%" x2="50.414%" y1="-33.211%" y2="100.08%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVwO" x1="50.034%" x2="50.034%" y1="-7.143%" y2="100.168%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVOb" x1="50.325%" x2="50.325%" y1="-220.199%" y2="147.927%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVKp" x1="49.159%" x2="49.159%" y1="-121.474%" y2="112.576%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVg3" x1="50.422%" x2="50.422%" y1="-94.484%" y2="103.334%"><stop offset="0%" stopColor="#f6871f"/><stop offset="23.93%" stopColor="#f57e20"/><stop offset="62.62%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-      <linearGradient id="PVfg" x1="49.931%" x2="49.931%" y1="0%" y2="100.017%"><stop offset="0%" stopColor="#f7941e"/><stop offset="0%" stopColor="#f68b1f"/><stop offset="0%" stopColor="#f6871f"/><stop offset="24.02%" stopColor="#f57e20"/><stop offset="63.06%" stopColor="#f36621"/><stop offset="100%" stopColor="#f04923"/></linearGradient>
-    </defs>
-    <path fill="url(#PVEm)" d="m109.97 81.482l-1.494-54.487c0-1.694-1.394-2.989-2.988-2.989c-1.693 0-2.988 1.295-2.988 2.989l-1.395 54.487l1.395 26.197c0 1.694 1.394 2.989 2.988 2.989c1.693 0 2.988-1.295 2.988-2.989z" />
-    <path fill="url(#PVIj)" d="m99.212 81.482l-1.693-56.58a2.784 2.784 0 0 0-2.789-2.788a2.784 2.784 0 0 0-2.79 2.789l-1.493 56.48l1.494 26.396a2.784 2.784 0 0 0 2.789 2.79a2.784 2.784 0 0 0 2.79-2.79z" />
-    <path fill="url(#PVF3)" d="M76.003 25.301c-.1-1.295-1.096-2.39-2.39-2.39s-2.292.996-2.391 2.39l-1.694 56.081l1.694 26.795c0 1.295 1.096 2.291 2.39 2.291s2.292-.996 2.391-2.39l1.992-26.796z" />
-    <path fill="url(#PVJ1)" d="M86.761 23.409c0-1.395-1.195-2.59-2.59-2.59a2.57 2.57 0 0 0-2.59 2.59l-1.593 57.973l1.593 26.596c0 1.395 1.196 2.59 2.59 2.59c1.395 0 2.49-1.096 2.59-2.59l1.793-26.596z" />
-    <path fill="url(#PVu9)" d="M32.473 109.87c.797 0 1.395-.597 1.494-1.493l2.59-26.995l-2.59-27.89c-.1-.798-.697-1.495-1.494-1.495s-1.394.598-1.494 1.494l-2.291 27.891l2.291 26.995c0 .896.697 1.494 1.494 1.494" />
-    <path fill="url(#PVlH)" d="m120.828 81.482l-1.395-64.747c0-1.096-.597-2.092-1.494-2.69c-.498-.299-1.095-.598-1.793-.598c-.597 0-1.195.2-1.793.499c-.896.597-1.494 1.593-1.494 2.689v.598l-1.195 64.15l1.195 25.998v.1c0 .696.299 1.394.797 1.892c.598.697 1.494 1.195 2.49 1.195c.897 0 1.694-.398 2.291-.896c.598-.598.996-1.395.996-2.291l.1-2.59z" />
-    <path fill="url(#PVcG)" d="M44.327 55.483c-.1-.996-.797-1.693-1.694-1.693s-1.693.697-1.693 1.693l-2.092 25.9l2.092 27.193c.1.996.797 1.693 1.693 1.693c.897 0 1.694-.697 1.694-1.693l2.39-27.194z" />
-    <path fill="url(#PV9b)" d="M52.893 37.354c-.996 0-1.892.797-1.892 1.893l-1.992 42.135L51 108.576c.1 1.096.896 1.892 1.892 1.892s1.893-.796 1.893-1.892l2.291-27.194l-2.291-42.135c0-.996-.897-1.893-1.893-1.893" />
-    <path fill="url(#PVb7)" d="m65.245 108.576l2.092-27.094l-2.092-51.798c-.1-1.195-.996-2.092-2.191-2.092c-1.196 0-2.092.897-2.192 2.092L58.97 81.482l1.892 27.094c0 1.195.996 2.092 2.192 2.092c1.195.1 2.092-.897 2.191-2.092" />
-    <path fill="url(#PVwO)" d="M128.697 7.87a3.44 3.44 0 0 0-1.793-.499a3.48 3.48 0 0 0-2.191.797c-.797.598-1.295 1.594-1.295 2.69v.398l-1.395 70.325l.698 12.95l.697 12.65c0 1.893 1.594 3.387 3.486 3.387s3.387-1.594 3.487-3.486l1.494-25.6l-1.494-70.624c-.1-1.295-.698-2.391-1.694-2.989" />
-    <path fill="url(#PVOb)" d="M2.889 96.324c.498 0 .896-.399.996-.996l2.191-13.946l-2.191-14.145c-.1-.597-.498-.996-.996-.996a1 1 0 0 0-.996.996L0 81.382l1.893 13.946c0 .597.498.996.996.996" />
-    <path fill="url(#PVKp)" d="M12.352 104.79c.498 0 .996-.398 1.095-.995l2.89-22.413l-2.89-22.91c-.1-.598-.498-.997-1.095-.997c-.498 0-.996.399-1.096.997l-2.49 22.91l2.49 22.413c.1.597.498.996 1.096.996" />
-    <path fill="url(#PVg3)" d="M22.313 108.875c.697 0 1.195-.498 1.295-1.295l2.69-26.198l-2.69-27.194c-.1-.697-.598-1.295-1.295-1.295s-1.195.498-1.295 1.295l-2.39 27.194l2.39 26.198c.1.697.598 1.295 1.295 1.295" />
-    <path fill="url(#PVfg)" d="M223.626 48.012c-4.283 0-8.367.897-12.152 2.391C208.984 22.213 185.276 0 156.389 0a55.1 55.1 0 0 0-20.022 3.785c-2.39.897-2.988 1.893-2.988 3.686v99.511c0 1.893 1.494 3.387 3.387 3.586h86.96c17.332 0 31.378-13.846 31.378-31.178c-.1-17.332-14.145-31.378-31.478-31.378" />
+  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" {...props}>
+    <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+    <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+    <path d="M10 9H8" />
+    <path d="M16 13H8" />
+    <path d="M16 17H8" />
   </svg>
 );
 

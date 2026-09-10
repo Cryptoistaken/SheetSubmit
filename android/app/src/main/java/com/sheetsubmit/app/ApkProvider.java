120e776e4913cfe7e3b7fee8608089c7f2d51d0d
Cryptoistaken <traderspopy@gmail.com>
2026-09-10 16:11:14 +0600

    android: applicationId org.brilliant.android -> com.sheetsubmit.app (provider authority too)

--- android/app/src/main/java/com/sheetsubmit/app/ApkProvider.java
+++ android/app/src/main/java/com/sheetsubmit/app/ApkProvider.java
@@ -26,7 +26,7 @@ import java.io.FileNotFoundException;
  */
 public class ApkProvider extends ContentProvider {
 
-    public static final String AUTHORITY = "org.brilliant.android.apk";
+    public static final String AUTHORITY = "com.sheetsubmit.app.apk";
     private static final String DIR = "apk";
 
     public static Uri uriFor(Context ctx, File file) {
